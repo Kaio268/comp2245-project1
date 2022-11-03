d1fa2e5f673519fcbaff72b740385239d7a175bb
Kaio <Makaio.Boodram@gmail.com>
2022-11-02 23:47:18 -0400

    Added initial starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 /* Add your JavaScript to this file */
+var starter=function(){
+    var email_entry = document.getElementById("email");
+    document.getElementsByClassName("btn")[1].onclick=function(e){
+         var input = email_entry.value;
+         if (input == "") {
+             document.getElementsByClassName("message")[0].innerHTML = "Please enter a valid email address.";
+         } 
+         else if {
+             document.getElementsByClassName("message")[0].innerHTML = "Thank you! Your email address " +input + " has been added to our mailing list!";
+         }
+         else  {
+         alert("Email address is invalid!");
+        document.form1.text1.focus();
+        return false;;
+     }
+ }
+}
+ 
+ window.onload=starter;
